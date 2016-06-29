a872d949cfdb2ef453eda49b4d827e460a7d456d
callmewhy <whywanghai@gmail.com>
2016-06-29 22:00:31 +0800

    buttons

--- VoiceDiary/General/View/CalenderView.swift
+++ VoiceDiary/General/View/CalenderView.swift
@@ -9,9 +9,43 @@
 import UIKit
 
 class CalenderView: UIView {
+    let progress = 0
+    let size = 31
+    var buttons:[UIView] = []
     
-    override func awakeFromNib() {
-        self.backgroundColor = UIColor.clearColor()
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        self.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        for _ in 0..<size {
+            let v = UIView()
+            v.backgroundColor = UIColor.yellowColor()
+            addSubview(v)
+            buttons.append(v)
+        }
+    }
+    
+    func update() {
+        let colNum = 7
+        let rowNum = size / 7 + 1
+        let itemWidth = CGFloat(10)
+        let itemHeight = CGFloat(10)
+        let itemSpacingW = (self.bounds.width - itemWidth * CGFloat(colNum)) / (CGFloat(colNum) - 1)
+        let itemSpacingH = (self.bounds.height - itemHeight * CGFloat(rowNum)) / (CGFloat(rowNum) - 1)
+        
+        for (i, button) in buttons.enumerate() {
+            let x = CGFloat(i % colNum) * (itemWidth + itemSpacingW)
+            let y = CGFloat(i / colNum) * (itemHeight + itemSpacingH)
+            button.frame = CGRectMake(x, y, itemWidth, itemHeight)
+        }
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        update()
+    }
+    
 }
