9d77e6ee5754bc9dd55614cc4bd669e3b52fe977
callmewhy <whywanghai@gmail.com>
2016-07-28 16:06:49 +0800

    use layer in record button

--- MrVoice/General/View/RecordButton.swift
+++ MrVoice/General/View/RecordButton.swift
@@ -9,51 +9,84 @@
 import UIKit
 import SwiftHEXColors
 
-@objc
 protocol RecordButtonHandler {
     func stateChanged(state: RecordButton.State)
 }
 
 class RecordButton: UIButton {
-    @objc
     enum State: Int {
         case Idle
         case Recording
         case Disabled
     }
     var currentState: State = State.Idle
-    var progress = 0
-    let alphas: [CGFloat] = [0.02, 0.04, 0.08]
-    var layerCount:  Int {
-        get {
-            return alphas.count
+    var delegate: RecordButtonHandler?
+    let spacing = 20
+    lazy var idleLayer: CAShapeLayer = {
+        let width = self.bounds.width
+        let margin = CGFloat(self.backgroundLayers.count * self.spacing)
+        let radius = width / 2 - margin
+        let path = UIBezierPath(ovalInRect: CGRectMake(margin, margin, radius*2, radius*2))
+        
+        let layer = CAShapeLayer()
+        layer.fillColor = UIColor.RecordButton.mainColor.CGColor
+        layer.path = path.CGPath
+        layer.shadowColor = layer.fillColor
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        return layer
+    }()
+    lazy var recordingLayer: CAShapeLayer = {
+        let width = self.bounds.width
+        let margin = CGFloat(self.backgroundLayers.count * self.spacing)
+        let radius = width / 2 - margin
+        let path = UIBezierPath(ovalInRect: CGRectMake(margin, margin, radius*2, radius*2))
+        
+        let layer = CAShapeLayer()
+        layer.fillColor = UIColor.redColor().CGColor
+        layer.path = path.CGPath
+        layer.shadowColor = layer.fillColor
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        return layer
+    }()
+    lazy var backgroundLayers: [CAShapeLayer] = {
+        let layers = [CAShapeLayer(), CAShapeLayer(), CAShapeLayer()]
+        let alphas:[CGFloat] = [0.02, 0.04, 0.08]
+        let width = self.bounds.width
+        
+        for (i, layer) in layers.enumerate() {
+            let margin = CGFloat(i * self.spacing)
+            let radius = width / 2 - margin
+            let path = UIBezierPath(ovalInRect: CGRectMake(margin, margin, radius*2, radius*2))
+            
+            layer.fillColor = UIColor(white: 1, alpha: alphas[i]).CGColor
+            layer.path = path.CGPath
         }
-    }
-    
-    @IBOutlet var delegate: RecordButtonHandler!
+        return layers
+    }()
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         addTarget(self, action: #selector(pressed), forControlEvents: .TouchUpInside)
+        setupLayers()
     }
 }
 
+// MARK: - Public
 extension RecordButton {
     func pressed(sender: UIButton) {
         switch currentState {
         case .Idle:
             currentState = .Recording
-            
-            progress = 0
-            setNeedsDisplay()
-            UIView.animateWithDuration(1, animations: {
-            }, completion: { _ in
-                self.progress = 1
-                self.setNeedsDisplay()
-            })
+            idleLayer.removeFromSuperlayer()
+            layer.addSublayer(recordingLayer)
         case .Recording:
             currentState = .Idle
-            setNeedsDisplay()
+            recordingLayer.removeFromSuperlayer()
+            layer.addSublayer(idleLayer)
         default:
             return
         }
@@ -63,62 +96,10 @@ extension RecordButton {
 
 // MARK: - Private
 extension RecordButton {
-    private func addLayer(index: Int, width: CGFloat, spacing: CGFloat) {
-        let context = UIGraphicsGetCurrentContext()
-        let margin = spacing * CGFloat(index)
-        let size = width - margin * 2
-        CGContextSetFillColorWithColor(context, UIColor(white: 1, alpha: alphas[index]).CGColor)
-        CGContextAddEllipseInRect(context, CGRectMake(margin, margin, size, size))
-        CGContextFillPath(context)
-    }
-    
-    private func drawIdle() {
-        let context = UIGraphicsGetCurrentContext()
-        CGContextSetFillColorWithColor(context, UIColor.RecordButton.mainColor.CGColor)
-        CGContextAddEllipseInRect(context, CGRectMake(60, 60, 80, 80))
-        CGContextFillPath(context)
-        for i in 0..<layerCount {
-            addLayer(i, width: 200, spacing: 20)
+    private func setupLayers() {
+        for i in backgroundLayers {
+            layer.addSublayer(i)
         }
-    }
-    
-    private func drawRecording() {
-        let context = UIGraphicsGetCurrentContext()
-        CGContextSetStrokeColorWithColor(context, UIColor.RecordButton.mainColor.CGColor)
-        CGContextSetLineWidth(context, 1.5)
-        CGContextAddEllipseInRect(context, CGRectMake(60, 60, 80, 80))
-        CGContextStrokePath(context)
-        
-        CGContextSetFillColorWithColor(context, UIColor.RecordButton.mainColor.colorWithAlphaComponent(0.2).CGColor)
-        CGContextAddEllipseInRect(context, CGRectMake(61.5, 61.5, 77, 77))
-        CGContextFillPath(context)
-        
-        CGContextSetFillColorWithColor(context,UIColor.RecordButton.mainColor.CGColor)
-        CGContextAddPath(context, UIBezierPath(roundedRect: CGRectMake(80,80,40,40), cornerRadius: 2.0).CGPath)
-        CGContextClosePath(context);
-        CGContextFillPath(context);
-        
-        for i in 0..<progress {
-            addLayer(layerCount - 1 - i, width: 200, spacing: 20)
-        }
-    }
-    
-    private func drawDisabled() {
-        
-    }
-}
-
-// MARK: - Override
-extension RecordButton {
-    override func drawRect(rect: CGRect) {
-        log.debug(progress)
-        switch currentState {
-        case .Idle:
-            drawIdle()
-        case .Recording:
-            drawRecording()
-        case .Disabled:
-            drawDisabled()
-        }
+        layer.addSublayer(idleLayer)
     }
 }
